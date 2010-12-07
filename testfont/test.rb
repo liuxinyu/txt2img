4719c2d8ceef5bfc3f28feb3b3f009f80bb0ab41
Liu Xinyu <liuxy.cn@gmail.com>
2010-12-07 21:17:49 +0800

    cmd line only

--- testfont/test.rb
+++ testfont/test.rb
@@ -1,11 +1,12 @@
 require 'rubygems'
 require 'RMagick'
 imagename='txt2img.png'
-img = Magick::ImageList.new(imagename)
-#img = Magick::Image.new(200,200,Magick::HatchFill.new('green','white')) 
+#img = Magick::ImageList.new(imagename)
+img = Magick::Image.new(400,200,Magick::HatchFill.new('green','green')) 
 gc= Magick::Draw.new
+gc.stroke('transparent') 
 gc.pointsize(28)
 gc.font='maozedong.ttf'
-gc.text(20,20,'好好学习，天天向上')
+gc.text(20,100,'濂藉ソ瀛︿範锛屽ぉ澶╁悜涓娿�傛瘺娉戒笢')
 gc.draw(img)
 img.write(imagename)
